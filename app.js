f5baf89ca2563b43139a91eda3248069f3582d32
Vincent McGargill <vincentmcgargill@gmail.com>
2020-08-19 19:37:52 -0600

    Created Employee class.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
