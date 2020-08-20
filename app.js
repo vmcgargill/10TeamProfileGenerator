cae4129b85c7cb33561deb2a1a23ce2cdb8045ac
Vincent McGargill <vincentmcgargill@gmail.com>
2020-08-19 23:12:45 -0600

    Huge milestone: all tests are now passing succesfully.

--- app.js
+++ app.js
@@ -10,10 +10,99 @@ const render = require("./lib/htmlRenderer");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+let EmployeeArray = new Array();
+
+const EmployeeQuestions = [
+    {
+        type: "input",
+        message: "Please enter the name of the employee:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Please enter the id of the employee:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Please enter the work email address of the employee:",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "Please select a role/position for this employee:",
+        name: "role",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    }
+]
+
+const ManagerQuestions = [
+    {
+        type: "input",
+        message: "Please enter the office number of the manager:",
+        name: "officeNumber"
+    }
+]
+
+const EngineerQuestions = [
+    {
+        type: "input",
+        message: "Please enter the GitHub user name of the engineer:",
+        name: "github"
+    }
+]
+
+const InternQuestions = [
+    {
+        type: "input",
+        message: "Please enter the school that the intern attends:",
+        name: "school"
+    }
+]
+
+inquirer.prompt(EmployeeQuestions).then((response) => {
+    let name = response.name;
+    let id = response.id;
+    let email = response.email;
+    let role = response.role;
+    let officeNumber;
+    let github;
+    let school;
+
+    if (role === 'Manager') {
+        inquirer.prompt(ManagerQuestions).then((response) => {
+            officeNumber = response.officeNumber;
+            let employee = new Manager.Manager(name, id, email, officeNumber);
+            EmployeeArray.push(employee);
+            console.log(EmployeeArray);
+        })
+    } else if (role === 'Engineer') {
+        inquirer.prompt(EngineerQuestions).then((response) => {
+            github = response.github;
+            let employee = new Engineer.Engineer(name, id, email, github);
+            EmployeeArray.push(employee);
+            console.log(EmployeeArray);
+        })
+    } else if (role === 'Intern') {
+        inquirer.prompt(InternQuestions).then((response) => {
+            school = response.school;
+            let employee = new Intern.Intern(name, id, email, school);
+            EmployeeArray.push(employee);
+            console.log(EmployeeArray);
+        })
+    }
+})
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// function render() {
+
+// }
+
+// init()
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
